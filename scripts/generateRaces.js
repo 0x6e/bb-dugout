7e227ea25155ab9a9954e2a9ec8c68add031a5f4
0x6e <0x6e@users.noreply.github.com>
2016-01-22 14:12:35 +0000

    generateRaces.js now generates all the app/races/ files bb-dugout requires from the files found in blood-bowl-resources

--- scripts/generateRaces.js
+++ scripts/generateRaces.js
@@ -21,6 +21,16 @@ for (i in raceFiles) {
     races.push(raceSummary)
     
     // Create the race details
+    var raceDetails = {
+        id: raceId,
+        name: race.name,
+        description: race.description,
+        rerollCap: race.rerollCap,
+        rerollPrice: race.rerollPrice,
+        playerTypes: race.players
+    }
+    
+    fs.writeFileSync( outputDir + raceId + '.json', JSON.stringify(raceDetails, null, '\t'), 'utf8')
 }
 
 fs.writeFileSync(outputDir + 'races.json', JSON.stringify(races, null, ' '), 'utf8')
