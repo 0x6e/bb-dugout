4bebfcd02f5290791c021a0f6c1a5e7451e9edd5
0x6e <0x6e@users.noreply.github.com>
2016-01-22 13:51:45 +0000

    generateRaces.js creates the app/races/races.json file from the team files in blood-bowl-resources.

---
+++ scripts/generateRaces.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+var resourcePath = '../blood-bowl-resources/'
+var outputDir = '../app/races/'
+
+var raceFiles = fs.readdirSync(resourcePath)
+
+var races = []
+
+for (i in raceFiles) {
+    var race = JSON.parse(fs.readFileSync(resourcePath + raceFiles[i], 'utf8'));
+    var raceId = race.name.toLowerCase()
+    raceId = raceId.replace(/([\s])+/g, '-')
+    
+    // Append the race summary
+    var raceSummary = {
+        id: raceId,
+        name: race.name,
+        description: race.description
+    }
+    races.push(raceSummary)
+    
+    // Create the race details
+}
+
+fs.writeFileSync(outputDir + 'races.json', JSON.stringify(races, null, ' '), 'utf8')
